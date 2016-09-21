51fcaa6d66619beecdee0536720fcd26343981df
mveritym <mveritym@gmail.com>
2016-09-20 21:15:36 -0700

    Removing token :grimacing:

--- src/queries/fetch.js
+++ src/queries/fetch.js
@@ -1,9 +1,10 @@
 import fetch from 'isomorphic-fetch';
+import { GITHUB_TOKEN } from '../constants/tokens';
 
 export default async function fetchQuery(query) {
   const response = await fetch('https://api.github.com/graphql', {
     method: 'POST',
-    headers: {'Authorization': 'bearer a50b3f8a62c7b95f89088f553ebf02895065e68e'},
+    headers: {'Authorization': `bearer ${GITHUB_TOKEN}`},
     body: JSON.stringify({query})
   });
 
