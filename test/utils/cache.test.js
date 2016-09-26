13e88fc0cf92970c9021e477389dae9506c727b2
mveritym <mveritym@gmail.com>
2016-09-26 15:53:27 +0100

    Tests for cache function

--- test/utils/cache.test.js
+++ test/utils/cache.test.js
@@ -1,5 +1,30 @@
-describe('cache', () => {
-  it('should add an item to the cache', () => {
+import Cache from '../../src/utils/cache';
 
+describe('cache', () => {
+  let cache, timeout;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    timeout = 1000;
+    cache = new Cache(timeout);
   });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  it('should add an item to the cache', () => {
+    cache.clearKey = jest.fn();
+    cache.set('test', 123);
+    expect(setInterval.mock.calls.length).toBe(1);
+    expect(setInterval.mock.calls[0]).toEqual([cache.clearKey('test'), timeout]);
+    expect(cache.clearKey).toBeCalledWith('test');
+  });
+
+  it('should clear the cache', () => {
+    cache.set('test', 123);
+    jest.runOnlyPendingTimers();
+    expect(clearInterval).toBeCalled();
+    expect(cache.isCached('test')).toBeFalsy();
+  })
 });
